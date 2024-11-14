e2af8869c79afe0ba44cb69067869fabd3b22f15
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-11-14 15:09:07 -0500

    fix: clientwrapper lint error

--- src/app/clientwrapper.tsx
+++ src/app/clientwrapper.tsx
@@ -1,7 +1,7 @@
-// prettier-ignore
-// eslint-disable-next-line no-unused-expressions
-// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-"use client"
-export function ClientWrapper({ children }) {
+// src/app/clientwrapper.tsx
+"use client";
+import { ReactNode } from "react";
+
+export function ClientWrapper({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
