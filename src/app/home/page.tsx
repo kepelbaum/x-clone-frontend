683851529521f937a50a71a050942ceb9fc9a8db
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-11-16 13:03:59 -0500

    fix: another attempt at localstorage fix

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { useAppState } from "../lib/context";
 import { useHomeFetch } from "../lib/fetch";
 import { Navbar } from "../navbar";
@@ -11,7 +9,7 @@ import { Rightsection } from "../lib/rightsection";
 import { useState } from "react";
 import { useLocalStorage } from "../lib/useLocalStorage";
 
-export default function Home() {
+export function HomeContent() {
   const { posts, users, updateCounter, active, setActive, follows } =
     useAppState();
   const user = useLocalStorage("username");
@@ -47,45 +45,50 @@ export default function Home() {
     } // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [localUpdateCounter]);
 
+  if (!posts || !users || !userAvatar || !user) return null;
+
   return (
-    posts &&
-    users &&
-    userAvatar &&
-    user && (
-      <div className="bg-black text-white min-h-screen fixed inset-0 overflow-y-auto overflow-x-hidden">
-        <div className="w-full max-w-[100vw] flex relative">
-          <div className="hidden md:block md:h-screen md:w-[calc((100vw-600px)/2)]"></div>
-          <Navbar />
-          <main className="w-full md:w-[600px] pb-16 md:pb-0 border-gray-600 border-2">
-            <TopHomeMenu />
-            <Messagebox avatar={userAvatar} />
-            {active === "foryou" &&
-              posts
-                .filter((post) => !post.ifreply)
-                .sort((a, b) => {
-                  return a.post_id > b.post_id ? -1 : 1;
-                })
-                .map((post) => <Tweet key={post.post_id} post={post} />)}
-            {active === "following" &&
-              posts
-                .filter(
-                  (post) =>
-                    !post.ifreply &&
-                    follows.notifications.some(
-                      (notification) =>
-                        notification.follower === user &&
-                        notification.following === post.username
-                    )
-                )
-                .sort((a, b) => {
-                  return a.post_id > b.post_id ? -1 : 1;
-                })
-                .map((post) => <Tweet key={post.post_id} post={post} />)}
-          </main>
-          <Rightsection />
-          <div className="hidden md:block md:h-screen md:w-[calc((100vw-600px)/2)]"></div>
-        </div>
+    <div className="bg-black text-white min-h-screen fixed inset-0 overflow-y-auto overflow-x-hidden">
+      <div className="w-full max-w-[100vw] flex relative">
+        <div className="hidden md:block md:h-screen md:w-[calc((100vw-600px)/2)]"></div>
+        <Navbar />
+        <main className="w-full md:w-[600px] pb-16 md:pb-0 border-gray-600 border-2">
+          <TopHomeMenu />
+          <Messagebox avatar={userAvatar} />
+          {active === "foryou" &&
+            posts
+              .filter((post) => !post.ifreply)
+              .sort((a, b) => {
+                return a.post_id > b.post_id ? -1 : 1;
+              })
+              .map((post) => <Tweet key={post.post_id} post={post} />)}
+          {active === "following" &&
+            posts
+              .filter(
+                (post) =>
+                  !post.ifreply &&
+                  follows.notifications.some(
+                    (notification) =>
+                      notification.follower === user &&
+                      notification.following === post.username
+                  )
+              )
+              .sort((a, b) => {
+                return a.post_id > b.post_id ? -1 : 1;
+              })
+              .map((post) => <Tweet key={post.post_id} post={post} />)}
+        </main>
+        <Rightsection />
+        <div className="hidden md:block md:h-screen md:w-[calc((100vw-600px)/2)]"></div>
       </div>
-    )
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeContent />
+    </Suspense>
   );
 }
