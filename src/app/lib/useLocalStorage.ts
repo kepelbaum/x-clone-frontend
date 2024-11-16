0a77611f2923857ca2ed4869a165833a4b284665
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-11-16 14:33:54 -0500

    fix: another try at localstorage logging/fix

--- src/app/lib/useLocalStorage.ts
+++ src/app/lib/useLocalStorage.ts
@@ -2,18 +2,19 @@
 
 import { useEffect, useState } from "react";
 
+const ISSERVER = typeof window === "undefined";
+const getStorageValue = (key: string) => {
+  if (ISSERVER) return null;
+  return localStorage.getItem(key);
+};
+
 export function useLocalStorage(key: string) {
   console.log("1. Hook is executing");
-  const ISSERVER = typeof window === "undefined";
-  console.log("2. ISSERVER is:", ISSERVER);
   const [value, setValue] = useState<string | null>(null);
 
   useEffect(() => {
-    console.log("3. Effect is running");
-    if (!ISSERVER) {
-      console.log("4. About to access localStorage");
-      setValue(localStorage.getItem(key));
-    }
+    console.log("2. Effect is running");
+    setValue(getStorageValue(key));
   }, [key]);
 
   return value;
