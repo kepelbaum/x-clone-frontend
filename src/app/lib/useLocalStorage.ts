e7ac0f83725b7a945c69ff75e927a17dc37be0bc
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-11-16 14:11:08 -0500

    fix: trying another fix for localstorage with window === undefined check

--- src/app/lib/useLocalStorage.ts
+++ src/app/lib/useLocalStorage.ts
@@ -6,7 +6,10 @@ export function useLocalStorage(key: string) {
   const [value, setValue] = useState<string | null>(null);
 
   useEffect(() => {
-    setValue(localStorage.getItem(key));
+    const ISSERVER = typeof window === "undefined";
+    if (!ISSERVER) {
+      setValue(localStorage.getItem(key));
+    }
   }, [key]);
 
   return value;
