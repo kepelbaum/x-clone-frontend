4a9436fe85dc53ebea17ac8b1d3d99b382563cd3
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-11-16 14:27:20 -0500

    fix: console.logs to try and identify the issue on vercel upload

--- src/app/lib/useLocalStorage.ts
+++ src/app/lib/useLocalStorage.ts
@@ -3,11 +3,15 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key: string) {
+  console.log("1. Hook is executing");
+  const ISSERVER = typeof window === "undefined";
+  console.log("2. ISSERVER is:", ISSERVER);
   const [value, setValue] = useState<string | null>(null);
 
   useEffect(() => {
-    const ISSERVER = typeof window === "undefined";
+    console.log("3. Effect is running");
     if (!ISSERVER) {
+      console.log("4. About to access localStorage");
       setValue(localStorage.getItem(key));
     }
   }, [key]);
