b5cfb2a59dab49dcc593582a3b74984854990d6f
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-12-23 17:28:18 -0500

    fix: different approach to image size trying to prevent it from being cut off on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,18 @@ export default function Home() {
   const { errors } = useAppState();
 
   return (
-    <div className="min-h-screen h-screen w-screen bg-black text-white flex flex-col md:flex-row justify-center items-center gap-10 md:overflow-hidden overflow-y-auto py-10 md:py-0">
-      <Image
-        src="/X_logo.jpg"
-        alt="X logo"
-        width={500}
-        height={500}
-        className="w-[150px] h-[150px] md:w-[350px] md:h-[350px] lg:w-[500px] lg:h-[500px]"
-      ></Image>
+    <div className="w-screen h-screen bg-black text-white flex flex-col md:flex-row justify-center items-center gap-10">
+      <div className="w-[150px] h-[150px] md:w-[350px] md:h-[350px] lg:w-[500px] lg:h-[500px] relative">
+        <Image
+          src="/X_logo.jpg"
+          alt="X logo"
+          fill
+          sizes="(max-width: 768px) 150px,
+       (max-width: 1024px) 350px,
+       500px"
+          className="object-contain"
+        ></Image>
+      </div>
       <div className="md:flex md:flex-col flex flex-col md:flex-row justify-center items-center gap-10">
         <p className="text-4xl font-bold">Happening Now</p>
         <p className="text-3xl font-bold">Join today.</p>
